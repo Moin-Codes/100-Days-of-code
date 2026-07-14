3f9d56582c8012823f43416c75e25c55fcbdbed4
Moin-Codes <shahmoin.as@gmail.com>
2026-07-14 23:50:12 +0530

    Majority Element = 2

---
+++ Day-16/majority_element.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int majorityElement(int nums[], int numsSize)
+{
+    int candidate = nums[0];
+    int count = 1;
+
+    for(int i = 1; i < numsSize; i++)
+    {
+        if(count == 0)
+        {
+            candidate = nums[i];
+        }
+
+        if(nums[i] == candidate)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+    }
+
+    return candidate;
+}
+
+int main()
+{
+    int nums[] = {2, 2, 1, 1, 1, 2, 2};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+
+    printf("Majority Element = %d\n", majorityElement(nums, numsSize));
+
+    return 0;
+}
